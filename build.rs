db7f6ca390e391e1f9775f79c478919b8ee721be
Steven Dee <git@wholezero.org>
2025-08-06 16:55:18 -0700

    Relicense as BSD
    Probably it is less misleading (especially given readpassphrase.c’s
    inclusion in BSD OSes) to just call the whole package BSD licensed than
    the odd chimera it was before.

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 // Copyright 2025 Steven Dee.
 //
-// This project is dual licensed under the MIT and Apache 2.0 licenses. See
-// the LICENSE file in the project root for details.
+// This project is made available under a BSD-compatible license. See the
+// LICENSE file in the project root for details.
 //
 // The readpassphrase source and header are copyright 2000-2002, 2007, 2010
 // Todd C. Miller.
