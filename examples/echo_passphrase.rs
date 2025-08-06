5e92e23c09383b01756651a6542eb80cd99d4a38
Steven Dee <git@wholezero.org>
2025-08-06 00:11:52 -0700

    OCD edit of copyright header

--- examples/echo_passphrase.rs
+++ examples/echo_passphrase.rs
@@ -1,10 +1,10 @@
 // Copyright 2025 Steven Dee.
 //
-// This project is dual licensed under the MIT and Apache 2.0 licenses. See the
-// LICENSE file in the project root for details.
+// This project is dual licensed under the MIT and Apache 2.0 licenses. See
+// the LICENSE file in the project root for details.
 //
-// The readpassphrase source and header are copyright 2000-2002, 2007, 2010 Todd
-// C. Miller.
+// The readpassphrase source and header are copyright 2000-2002, 2007, 2010
+// Todd C. Miller.
 
 use readpassphrase_3::{RppFlags, readpassphrase};
 
