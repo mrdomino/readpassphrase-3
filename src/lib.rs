f65204e5114cea06813ee05c431fe1d93ce62c14
Steven Dee <git@wholezero.org>
2025-08-08 16:38:43 -0700

    Full license in lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,24 @@
-// Copyright 2025 Steven Dee.
+// Copyright 2025
+//	Steven Dee
 //
-// This project is made available under a BSD-compatible license. See the
-// LICENSE file in the project root for details.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
+// are met:
 //
-// The readpassphrase source and header are copyright 2000-2002, 2007, 2010
-// Todd C. Miller.
+// Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// THIS SOFTWARE IS PROVIDED BY STEVEN DEE “AS IS” AND ANY EXPRESS
+// OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL STEVEN DEE BE LIABLE FOR ANY
+// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+// IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+// IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 //! This library endeavors to expose a thin wrapper around OpenBSD’s [`readpassphrase(3)`][0]
 //! function.
