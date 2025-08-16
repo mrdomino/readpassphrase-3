8f641cf3dd8f0a146d731e228c52d695da5a9480
Steven Dee <git@wholezero.org>
2025-08-16 08:11:46 -0700

    Show example of renaming import of Flags

--- examples/inplace.rs
+++ examples/inplace.rs
@@ -8,18 +8,18 @@
 
 use std::process::exit;
 
-use readpassphrase_3::{Flags, PASSWORD_LEN, readpassphrase};
+use readpassphrase_3::{Flags as RpFlags, PASSWORD_LEN, readpassphrase};
 use zeroize::Zeroizing;
 
 fn main() {
     let mut buf = Zeroizing::new(vec![0u8; PASSWORD_LEN]);
     let password = Zeroizing::new(
-        readpassphrase(c"Password: ", &mut buf, Flags::empty())
+        readpassphrase(c"Password: ", &mut buf, RpFlags::empty())
             .expect("failed reading passphrase")
             .to_string(),
     );
     for _ in 0..5 {
-        let confirm = readpassphrase(c"Confirmation: ", &mut buf, Flags::REQUIRE_TTY)
+        let confirm = readpassphrase(c"Confirmation: ", &mut buf, RpFlags::REQUIRE_TTY)
             .expect("failed reading confirmation");
         if *password == confirm {
             eprintln!("Passwords match.");
