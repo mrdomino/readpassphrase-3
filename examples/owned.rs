c7df8406869763795b449c253d3f773c6ce685cc
Steven Dee <git@wholezero.org>
2025-09-09 21:10:16 -0700

    Relicense as MIT OR Apache-2.0

--- examples/owned.rs
+++ examples/owned.rs
@@ -1,11 +1,3 @@
-// Copyright 2025 Steven Dee.
-//
-// This project is made available under a BSD-compatible license. See the
-// LICENSE file in the project root for details.
-//
-// The readpassphrase source and header are copyright 2000-2002, 2007, 2010
-// Todd C. Miller.
-
 use readpassphrase_3::{readpassphrase, readpassphrase_into, Error, Flags, PASSWORD_LEN};
 use zeroize::{Zeroize, Zeroizing};
 
